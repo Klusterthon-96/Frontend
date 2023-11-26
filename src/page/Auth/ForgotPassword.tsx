7d13f173e4d6d6a82277395f238941e692132351
Joshua <ejembijoshman@gmail.com>
2023-11-26 21:46:48 +0100

    shey e fe kpa mi ni part 2

--- src/page/Auth/ForgotPassword.tsx
+++ src/page/Auth/ForgotPassword.tsx
@@ -60,7 +60,7 @@ function ForgotPassword() {
       {open && (
         <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/90 overflow-hidden h-screen w-full outline-none font-sans">
           <div className="pointer-events-none relative flex m-2 min-h-screen w-auto translate-y-[-50px] items-center transition-all duration-300 ease-in-out md:my-7 md:mx-auto md:h-auto">
-            <div className="pointer-events-auto relative bg-clip-padding lg:w-[450px] mx-auto text-white text-center">
+            <div className="pointer-events-auto relative bg-clip-padding lg:w-[400px] p-4 mx-auto bg-white rounded-lg text-center">
               <div className="flex items-center justify-end mb-8">
                 <button
                   type="button"
@@ -68,7 +68,7 @@ function ForgotPassword() {
                   aria-label="Close"
                   onClick={() => setOpen(false)}
                 >
-                  <AiOutlineClose size={26} color="#FFF" />
+                  <AiOutlineClose size={26} color="#000" />
                 </button>
               </div>
               <div className="">
