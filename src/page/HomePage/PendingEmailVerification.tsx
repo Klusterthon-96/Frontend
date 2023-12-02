fdd1858341288569a9c9df6bc3385c5e5fbd9827
Candy <obiijeoma32@gmail.com>
2023-12-02 12:20:22 +0100

    updated the pending email component

--- src/page/HomePage/PendingEmailVerification.tsx
+++ src/page/HomePage/PendingEmailVerification.tsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
-import Button from "../../components/Button";
-import axios from "axios";
 import { useAuth } from "../../Context/authContext";
+import { FaArrowRight } from "react-icons/fa";
 import Swal from "sweetalert2";
+import axios from "axios";
+import pendingIcon from "../../asset/pendingConfirmation.svg";
 
 function PendingEmailVerification() {
-  const [backgroundColor, setBackgroundColor] = useState<string>("#006400");
-  console.log(setBackgroundColor);
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
   const { user } = useAuth();
 
-  const token = user?.data.accessToken;
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setButtonDisabled(true);
+    }, 20000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  const token = user?.data?.accessToken;
+  console.log(token);
   const resendEmailVerification = async () => {
     try {
       const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/auth/email`, {
@@ -34,27 +43,34 @@ function PendingEmailVerification() {
         icon: "error",
         text: `${error.response.data.message}`,
       });
-      console.error("Error sending email verification:", error);
+      console.log("Button clicked!");
+      if (isButtonDisabled) return;
+
+      setButtonDisabled(true);
+
+      // console.error("Error sending email verification:", error);
     }
   };
 
-  const [isButtonDisabled, setButtonDisabled] = useState(false);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setButtonDisabled(false);
-    }, 60000);
-
-    return () => clearTimeout(timeoutId);
-  }, [isButtonDisabled]);
   return (
     <>
       <div className="font-sans px-5 pb-5 grid place-items-center h-[90vh] lg:max-w-[450px] mx-auto">
         <div className=" text-center">
           <h2 className="text-[28px] text-center font-[600]">Email Confirmation</h2>
+          <div className=" w-full flex justify-center items-center">
+            <img className="" src={pendingIcon} alt="icon" />
+          </div>
           <p className="text-[20px] text-center mt-[20px] mb-[12px] font-[400]">We've sent a confirmation email to your inbox. If you haven't received it, please click the button below.</p>
           <div className="flex justify-center items-center">
-            <Button backgroundColor={backgroundColor} title={"Resend Confirmation Email"} onClick={resendEmailVerification} disabled={isButtonDisabled} />
+            <button
+              title={"Resend Confirmation Email"}
+              className={isButtonDisabled ? "px-3 py-2 flex items-center text-white bg-[#006400] bg-opacity-50 cursor-not-allowed rounded-[32px]" : "px-3 py-2 flex items-center text-white bg-[#006400] rounded-[32px]"}
+              onClick={resendEmailVerification}
+              disabled={isButtonDisabled}
+            >
+              Resend Confirmation Email
+              <FaArrowRight className="ml-3" />
+            </button>
           </div>
         </div>
       </div>
